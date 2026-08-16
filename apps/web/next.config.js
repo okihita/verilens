0b706bbc021dbe02985365016a927a11dc09f6d5
Okihita Sihaloho <okihita@gmail.com>
2026-08-16 12:22:13 +0700

    chore: disable Next.js development toolbar button in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@verilens/shared']
+  transpilePackages: ['@verilens/shared'],
+  devIndicators: false
 };
 
 module.exports = nextConfig;
